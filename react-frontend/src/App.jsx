f631bc160d8b10e33e511f626ac15fd9532f74c0
n3vsk1y <alexafanasenko900@gmail.com>
2025-01-03 16:40:09 +0300

    loading component

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -9,94 +9,112 @@ import Dashboard from './components/Dashboard/Dashboard'
 import Trades from './components/Trades/Trades'
 import Resources from './components/Resources/Resources'
 import Profile from './components/Profile/Profile'
+import Loading from './components/Loading/Loading'
 
 import { UserProvider } from './providers/UserContext'
 import ProtectedRoute from './providers/ProtectedRoute'
+
+function AppRotes() {
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <Helmet>
+                            <title>Добро пожаловать</title>
+                        </Helmet>
+                        <RootScreen />
+                    </>
+                }
+            />
+            <Route
+                path="/login"
+                element={
+                    <>
+                        <Helmet>
+                            <title>Вход</title>
+                        </Helmet>
+                        <LogInScreen />
+                    </>
+                }
+            />
+            <Route
+                path="/signup"
+                element={
+                    <>
+                        <Helmet>
+                            <title>Регистрация</title>
+                        </Helmet>
+                        <SignUpScreen />
+                    </>
+                }
+            />
+            <Route
+                path="/dashboard"
+                element={
+                    <ProtectedRoute>
+                        <Helmet>
+                            <title>Дашборд</title>
+                        </Helmet>
+                        <Dashboard />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/trades"
+                element={
+                    <ProtectedRoute>
+                        <Helmet>
+                            <title>Сделки</title>
+                        </Helmet>
+                        <Trades />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/resources"
+                element={
+                    <ProtectedRoute>
+                        <Helmet>
+                            <title>Полезные материалы</title>
+                        </Helmet>
+                        <Resources />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/profile"
+                element={
+                    <ProtectedRoute>
+                        <Helmet>
+                            <title>Профиль</title>
+                        </Helmet>
+                        <Profile />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/load"
+                element={
+                    <ProtectedRoute>
+                        <Helmet>
+                            <title>Загрузка...</title>
+                        </Helmet>
+                        <Loading />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    )
+}
 
 function App() {
     return (
         <HelmetProvider>
             <UserProvider>
                 <Router>
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                                <>
-                                    <Helmet>
-                                        <title>Добро пожаловать</title>
-                                    </Helmet>
-                                    <RootScreen />
-                                </>
-                            }
-                        />
-                        <Route
-                            path="/login"
-                            element={
-                                <>
-                                    <Helmet>
-                                        <title>Вход</title>
-                                    </Helmet>
-                                    <LogInScreen />
-                                </>
-                            }
-                        />
-                        <Route
-                            path="/signup"
-                            element={
-                                <>
-                                    <Helmet>
-                                        <title>Регистрация</title>
-                                    </Helmet>
-                                    <SignUpScreen />
-                                </>
-                            }
-                        />
-                        <Route
-                            path="/dashboard"
-                            element={
-                                <ProtectedRoute>
-                                    <Helmet>
-                                        <title>Дашборд</title>
-                                    </Helmet>
-                                    <Dashboard />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/trades"
-                            element={
-                                <ProtectedRoute>
-                                    <Helmet>
-                                        <title>Сделки</title>
-                                    </Helmet>
-                                    <Trades />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/resources"
-                            element={
-                                <ProtectedRoute>
-                                    <Helmet>
-                                        <title>Полезные материалы</title>
-                                    </Helmet>
-                                    <Resources />
-                                </ProtectedRoute>
-                            }
-                        />
-                        <Route
-                            path="/profile"
-                            element={
-                                <ProtectedRoute>
-                                    <Helmet>
-                                        <title>Профиль</title>
-                                    </Helmet>
-                                    <Profile />
-                                </ProtectedRoute>
-                            }
-                        />
-                    </Routes>
+                    <AppRotes />
                 </Router>
             </UserProvider>
         </HelmetProvider>
---
+++ react-frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Loading.scss'
+
+const Loading = () => {
+    return (
+        <div className="container">
+            <div className="loading-box">...</div>
+            <div className="loading-box">L</div>
+            <div className="loading-box">O</div>
+            <div className="loading-box">A</div>
+            <div className="loading-box">D</div>
+        </div>
+    )
+}
+
+export default Loading

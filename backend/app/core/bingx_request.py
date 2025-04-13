ed9b03f3dfe3c03570358c1fb8fbd88f757273c9
n3vsk1y <alexafanasenko900@gmail.com>
2025-04-13 19:27:41 +0300

    get trades fix

--- backend/app/core/bingx_request.py
+++ backend/app/core/bingx_request.py
@@ -3,11 +3,6 @@ import hmac
 from hashlib import sha256
 import requests
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 API_URL = "https://open-api.bingx.com"
 
 def get_server_time():
@@ -20,47 +15,38 @@ def get_server_time():
     return response["data"]["serverTime"]
 
 
-def get_trades(api_key: str, api_secret: str, symbol: str, start_time: int, end_time: int = int(time.time() * 1000)):
+def get_trades(api_key: str, api_secret: str, symbol: str, start_time: int, end_time: int=None):
     payload = {}
     path = '/openApi/swap/v1/trade/positionHistory'
     method = "GET"
-    params = {
-        'symbol': symbol + '-USDT',
-        'startTs': str(start_time),
-        'endTs': str(end_time),
+    paramsMap = {
+        "symbol": symbol + '-USDT',
+        "startTs": start_time,
+        "endTs": get_server_time() if end_time is None else end_time,
     }
+    paramsStr = parseParam(paramsMap)
+    return send_request(method, path, paramsStr, payload, api_key, api_secret)
 
-    params_str = parse_param(params)
-    return send_request(method, path, params_str, payload, api_key, api_secret)
 
-
-def get_sign(secret, payload):
-    signature = hmac.new(secret.encode("utf-8"),
+def get_sign(api_secret, payload):
+    signature = hmac.new(api_secret.encode("utf-8"),
                          payload.encode("utf-8"), digestmod=sha256).hexdigest()
     return signature
 
 
-def send_request(method, path, urlpa, payload, api_key, api_secret):
-    url = "%s%s?%s&signature=%s" % (
-        API_URL, path, urlpa, get_sign(api_secret, urlpa))
+def send_request(method, path, urlpa, payload, APIKEY, SECRETKEY):
+    url = f'https://open-api.bingx.com{path}?{urlpa}&signature={get_sign(SECRETKEY, urlpa)}'
     headers = {
-        'X-BX-APIKEY': api_key,
+        'X-BX-APIKEY': APIKEY,
     }
     response = requests.request(method, url, headers=headers, data=payload)
+    return response.json()
 
-    if response.status_code == 200:
-        return response.json()
+
+def parseParam(paramsMap):
+    sortedKeys = sorted(paramsMap)
+    paramsStr = "&".join(["%s=%s" % (x, paramsMap[x]) for x in sortedKeys])
+    if paramsStr != "":
+        return paramsStr + "&timestamp="+str(int(time.time() * 1000))
     else:
-        return {
-            "error": f"Request failed with status {response.status_code}",
-            "details": response.text
-        }
-
-
-def parse_param(paramsMap):
-    sorted_keys = sorted(paramsMap)
-    params_str = "&".join(["%s=%s" % (x, paramsMap[x]) for x in sorted_keys])
-    if params_str != "":
-        return params_str + "&timestamp="+str(int(time.time() * 1000))
-    else:
-        return params_str + "timestamp="+str(int(time.time() * 1000))
+        return paramsStr + "timestamp="+str(int(time.time() * 1000))
